6dab56e261bd4015d943b56e73079d314666204c
Victor Kropp <victor@kropp.name>
2012-01-22 18:35:44 +0100

    Restore functionality

--- src/name/kropp.diceroller/DiceRollActivity.java
+++ src/name/kropp.diceroller/DiceRollActivity.java
@@ -1,7 +1,14 @@
 package name.kropp.diceroller;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -9,11 +16,121 @@ import android.widget.TextView;
  * User: kropp
  */
 public class DiceRollActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private Dice6 myWhiteDice;
+    private RedDice6 myRedDice;
+    private SettlersOfCatanCitiesAndKnightsEventDice myEventDice;
+    private ShakeListener myShaker;
+    private boolean myVibeAfterRoll;
+    private Vibrator myVibrator;
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the DiceRoll tab");
-        setContentView(textview);
+    public DiceRollActivity() {
+        long seed = System.currentTimeMillis();
+        myWhiteDice = new Dice6(seed);
+        myRedDice = new RedDice6(seed * System.currentTimeMillis());
+        myEventDice = new SettlersOfCatanCitiesAndKnightsEventDice(seed * seed * System.currentTimeMillis());
+    }
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diceroller);
+
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                RollDice();
+            }
+        });
+
+        myVibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        myVibeAfterRoll = preferences.getBoolean("vibe_preference", false);
+        boolean rollOnShakeEnabled = preferences.getBoolean("shake_preference", false);
+        if (rollOnShakeEnabled) {
+            listenShakeEvent();
+        }
+
+        preferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                if (s.equals("shake_preference"))
+                {
+                    boolean value = sharedPreferences.getBoolean(s, false);
+                    if (value) {
+                        if (myShaker == null)
+                            listenShakeEvent();
+                        else
+                            myShaker.resume();
+                    }
+                    else
+                    {
+                        if (myShaker != null)
+                            myShaker.pause();
+                    }
+                }
+                if (s.equals("vibe_preference"))
+                {
+                    myVibeAfterRoll = sharedPreferences.getBoolean("vibe_preference", false);
+                }
+            }
+        });
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+        textView.setTextSize(20);
+    }
+
+    private void listenShakeEvent() {
+        myShaker = new ShakeListener(this);
+        myShaker.setOnShakeListener(new ShakeListener.OnShakeListener() {
+            public void onShake() {
+                RollDice();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        if (myShaker != null) {
+            myShaker.resume();
+        }
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        if (myShaker != null) {
+            myShaker.pause();
+        }
+        super.onPause();
+    }
+
+    private void RollDice() {
+        myWhiteDice.roll();
+        myRedDice.roll();
+        myEventDice.roll();
+
+        updateDiceImage(R.id.white_dice, myWhiteDice.getIconId());
+        updateDiceImage(R.id.red_dice, myRedDice.getIconId());
+        updateDiceImage(R.id.event_dice, myEventDice.getIconId());
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+
+        String[] lines = textView.getText().toString().split("\n", -1);
+        StringBuilder old = new StringBuilder();
+        for (int i = 0; i < 5; i++)
+            old.append(lines[i]).append("\n");
+
+        CharSequence newText = String.format("%d [%d] = %d %s\n%s", myWhiteDice.getCurrentValue(), myRedDice.getCurrentValue(), myWhiteDice.getCurrentValue() + myRedDice.getCurrentValue(), myEventDice.getCurrentEvent().toString(), old.toString());
+        textView.setText(newText);
+
+        if (myVibeAfterRoll)
+        {
+            myVibrator.vibrate(500);
+        }
+    }
+
+    private void updateDiceImage(int dice, int icon) {
+        final ImageView imageView = (ImageView) findViewById(dice);
+        imageView.setImageResource(icon);
     }
 }
--- src/name/kropp.diceroller/MyMainActivity.java
+++ src/name/kropp.diceroller/MyMainActivity.java
@@ -1,41 +1,18 @@
 package name.kropp.diceroller;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.app.TabActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Vibrator;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TabHost;
-import android.widget.TextView;
 
 public class MyMainActivity extends TabActivity
 {
-    private Dice6 myWhiteDice;
-    private RedDice6 myRedDice;
-    private SettlersOfCatanCitiesAndKnightsEventDice myEventDice;
-    private ShakeListener myShaker;
-    private boolean myVibeAfterRoll;
-    private Vibrator myVibrator;
-
     static final int ABOUT_DIALOG_ID = 1;
-    
-    public MyMainActivity() {
-        long seed = System.currentTimeMillis();
-        myWhiteDice = new Dice6(seed);
-        myRedDice = new RedDice6(seed * System.currentTimeMillis());
-        myEventDice = new SettlersOfCatanCitiesAndKnightsEventDice(seed * seed * System.currentTimeMillis());
-    }
 
     /** Called when the activity is first created. */
     @Override
@@ -67,80 +44,6 @@ public class MyMainActivity extends TabActivity
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(0);
-    }
-
-/*    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        final Button button = (Button) findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                RollDice();
-            }
-        });
-
-        myVibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        myVibeAfterRoll = preferences.getBoolean("vibe_preference", false);
-        boolean rollOnShakeEnabled = preferences.getBoolean("shake_preference", false);
-        if (rollOnShakeEnabled) {
-            listenShakeEvent();
-        }
-
-        preferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-                if (s.equals("shake_preference"))
-                {
-                    boolean value = sharedPreferences.getBoolean(s, false);
-                    if (value) {
-                        if (myShaker == null)
-                            listenShakeEvent();
-                        else
-                            myShaker.resume();
-                    }
-                    else
-                    {
-                        if (myShaker != null)
-                            myShaker.pause();
-                    }
-                }
-                if (s.equals("vibe_preference"))
-                {
-                    myVibeAfterRoll = sharedPreferences.getBoolean("vibe_preference", false);
-                }
-            }
-        });
-
-        final TextView textView = (TextView) findViewById(R.id.text);
-        textView.setTextSize(20);
-    }*/
-
-    private void listenShakeEvent() {
-        myShaker = new ShakeListener(this);
-        myShaker.setOnShakeListener(new ShakeListener.OnShakeListener() {
-            public void onShake() {
-                RollDice();
-            }
-        });
-    }
-
-    @Override
-    public void onResume() {
-        if (myShaker != null) {
-            myShaker.resume();
-        }
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        if (myShaker != null) {
-            myShaker.pause();
-        }
-        super.onPause();
     }
 
     @Override
@@ -190,37 +93,5 @@ public class MyMainActivity extends TabActivity
 
     private void openOptions() {
         this.startActivity(new Intent(this, MyPreferencesActivity.class));
-    }
-
-    private void RollDice() {
-        myWhiteDice.roll();
-        myRedDice.roll();
-        myEventDice.roll();
-
-/*
-        updateDiceImage(R.id.white_dice, myWhiteDice.getIconId());
-        updateDiceImage(R.id.red_dice, myRedDice.getIconId());
-        updateDiceImage(R.id.event_dice, myEventDice.getIconId());
-*/
-
-        final TextView textView = (TextView) findViewById(R.id.text);
-
-        String[] lines = textView.getText().toString().split("\n", -1);
-        StringBuilder old = new StringBuilder();
-        for (int i = 0; i < 5; i++)
-            old.append(lines[i]).append("\n");
-
-        CharSequence newText = String.format("%d [%d] = %d %s\n%s", myWhiteDice.getCurrentValue(), myRedDice.getCurrentValue(), myWhiteDice.getCurrentValue() + myRedDice.getCurrentValue(), myEventDice.getCurrentEvent().toString(), old.toString());
-        textView.setText(newText);
-        
-        if (myVibeAfterRoll)
-        {
-            myVibrator.vibrate(500);
-        }
-    }
-
-    private void updateDiceImage(int dice, int icon) {
-        final ImageView imageView = (ImageView) findViewById(dice);
-        imageView.setImageResource(icon);
     }
 }
