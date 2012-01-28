3d747b5a7bdcbcb76478a87b389a29ee49502088
Victor Kropp <victor@kropp.name>
2012-01-28 22:17:48 +0100

    DiceSet introduced

--- src/name/kropp.diceroller/StatsManager.java
+++ src/name/kropp.diceroller/StatsManager.java
@@ -1,5 +1,7 @@
 package name.kropp.diceroller;
 
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: kropp
@@ -26,13 +28,6 @@ public class StatsManager {
         return ourInstance;
     }
 
-    public void updateStats(int whiteDice, int redDice, Event eventDice) {
-        for (int i = MAX_LINES - 1; i > 0; i--)
-            myLines[i] = myLines[i-1];
-
-        myLines[0] = String.format("%d [%d] = %d %s", whiteDice, redDice, whiteDice + redDice, eventDice);
-    }
-
     public CharSequence getText()
     {
         StringBuilder builder = new StringBuilder();
@@ -40,5 +35,17 @@ public class StatsManager {
             builder.append(myLines[i]).append('\n');
         }
         return builder.toString();
+    }
+
+    public void updateStats(DiceSet diceSet) {
+        for (int i = MAX_LINES - 1; i > 0; i--)
+            myLines[i] = myLines[i-1];
+
+        List<Dice> dices = diceSet.getDices();
+        int whiteDice = dices.get(0).getCurrentValue();
+        int redDice = dices.get(1).getCurrentValue();
+        Event eventDice = ((SettlersOfCatanCitiesAndKnightsEventDice)dices.get(2)).getCurrentEvent();
+
+        myLines[0] = String.format("%d [%d] = %d %s", whiteDice, redDice, whiteDice + redDice, eventDice);
     }
 }
