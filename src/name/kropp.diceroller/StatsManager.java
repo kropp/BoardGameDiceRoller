ad1c21ea7d013abbeca7050cdcd6c3f9f8370649
Victor Kropp <victor@kropp.name>
2012-01-29 12:38:15 +0100

    Restore simple stats

--- src/name/kropp.diceroller/StatsManager.java
+++ src/name/kropp.diceroller/StatsManager.java
@@ -10,7 +10,7 @@ public class StatsManager {
     private static StatsManager ourInstance;
 
     final static int MAX_LINES = 20;
-    
+
     private String[] myLines = new String[MAX_LINES];
 
     private StatsManager() {
@@ -18,18 +18,15 @@ public class StatsManager {
             myLines[i] = "\n";
     }
 
-    public static StatsManager getInstance()
-    {
-        if (ourInstance == null)
-        {
+    public static StatsManager getInstance() {
+        if (ourInstance == null) {
             // not thread-safe, consider rewriting
             ourInstance = new StatsManager();
         }
         return ourInstance;
     }
 
-    public CharSequence getText()
-    {
+    public CharSequence getText() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < MAX_LINES; i++) {
             builder.append(myLines[i]).append('\n');
@@ -39,15 +36,28 @@ public class StatsManager {
 
     public void updateStats(DiceSet diceSet) {
         for (int i = MAX_LINES - 1; i > 0; i--)
-            myLines[i] = myLines[i-1];
+            myLines[i] = myLines[i - 1];
 
-/*
+        StringBuilder builder = new StringBuilder();
+        int sum = 0;
+
         List<Dice> dices = diceSet.getDices();
-        int whiteDice = dices.get(0).getCurrentValue();
-        int redDice = dices.get(1).getCurrentValue();
-        Event eventDice = ((SettlersOfCatanCitiesAndKnightsEventDice)dices.get(2)).getCurrentEvent();
+        for (Dice die : dices) {
+            if (die instanceof SettlersOfCatanCitiesAndKnightsEventDice) {
+                builder.append(' ').append(((SettlersOfCatanCitiesAndKnightsEventDice) die).getCurrentEvent());
+            } else {
+                if (die instanceof RedDice6) {
+                    builder.append(" [").append(die.getCurrentValue()).append(']');
+                } else {
+                    builder.append(' ').append(die.getCurrentValue());
+                }
 
-        myLines[0] = String.format("%d [%d] = %d %s", whiteDice, redDice, whiteDice + redDice, eventDice);
-*/
+                sum += die.getCurrentValue();
+            }
+        }
+
+        builder.append(" = ").append(sum);
+
+        myLines[0] = builder.toString();
     }
 }
