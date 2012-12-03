c3c9eb92a6860985836100e714b72e32b9800821
Victor Kropp <victor@kropp.name>
2012-12-03 22:32:46 +0100

    Keep screen on

--- src/name/kropp.diceroller/activities/DiceRollActivity.java
+++ src/name/kropp.diceroller/activities/DiceRollActivity.java
@@ -1,9 +1,13 @@
 package name.kropp.diceroller.activities;
 
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
+import android.view.WindowManager;
 import name.kropp.diceroller.R;
+import name.kropp.diceroller.settings.PreferenceNames;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,6 +18,12 @@ public class DiceRollActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean keepScreenOn = preferences.getBoolean(PreferenceNames.KeepScreenOn, false);
+        if (keepScreenOn) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             // If the screen is now in landscape mode, we can show the
--- src/name/kropp.diceroller/settings/PreferenceNames.java
+++ src/name/kropp.diceroller/settings/PreferenceNames.java
@@ -8,5 +8,7 @@ public final class PreferenceNames {
     public static final String SelectedGameId = "selected_game_id";
     public static final String SelectedSetId = "selected_set_id";
     public static final String Vibe = "vibe_preference";
+    public static final String Notification = "notification_preference";
     public static final String Shake = "shake_preference";
+    public static final String KeepScreenOn = "keep_screen_on_preference";
 }
